0d0f39f46e6e94074126166ba8cd8c7a407da9ae
Alex Teshome <alex_teshome@hotmail.com>
2019-11-16 15:44:52 -0500

    fixed cors issue

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.use(
   })
 );
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 app.post("/send", (req, res, next) => {
   var creds = req.body.creds;
   var name = req.body.name;
